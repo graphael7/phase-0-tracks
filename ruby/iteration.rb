a457a4e3d92d06464b4bd5cc8117ad1aba4c2511
Gibral Raphael <gibral.raphael@gmail.com>
2016-10-12 13:20:50 -0400

    added array and hashes to iteration file

---
+++ ruby/iteration.rb
@@ -0,0 +1,15 @@
+animals = ["dog","cat","lion", "tiger"]
+
+actors = {
+	name:  "Will Smith",
+	age:  43,
+	movie: "Men in Black"
+}
+
+animals.each do |animal|
+	puts animal
+end
+
+actors.each do |label,bio|
+	puts "This #{label} of this actor is #{bio}"
+end
