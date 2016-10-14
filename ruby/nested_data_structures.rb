c135f4d6f33b9df4c72d7506735aba8d1b59e44d
Gibral Raphael <gibral.raphael@gmail.com>
2016-10-14 14:29:25 -0400

    added printout statements for my nested data structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,32 +2,37 @@ classroom = {
 
 	desk1: {
 		supplies:[
-			'scissors','crayons']
+			'scissors','crayons'],
 		location: {
-			row: 1
+			row: 1,
 			column:4
-		}
+		},
 		color: 'blue'
 
-	}
+	},
 	desk2:{
 		supplies:[
-			'paper','pencil']
+			'paper','pencil'],
 		location: {
-			row: 3
+			row: 3,
 			column:2
-		}
+		},
 		color: 'red'
 
-	}
+	},
 	desk3:{
 		supplies:[
-			'pen','calculator']
+			'pen','calculator'],
 		location: {
-			row: 2
+			row: 2,
 			column:3
-		}
+		},
 		color: 'yellow'
 
 	}
 }
+
+puts classroom[:desk2][:supplies][1].capitalize
+puts classroom[:desk3][:color] = 'purple'
+puts classroom[:desk1][:location][:row]
+puts classroom[:desk3][:color]
