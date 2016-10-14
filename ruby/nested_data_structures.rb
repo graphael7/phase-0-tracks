fa06525476a0c5ed4ca9d0511c41edee2321b248
Gibral Raphael <gibral.raphael@gmail.com>
2016-10-14 14:20:47 -0400

    added nested data structures about classroom

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,33 @@
+classroom = {
+
+	desk1: {
+		supplies:[
+			'scissors','crayons']
+		location: {
+			row: 1
+			column:4
+		}
+		color: 'blue'
+
+	}
+	desk2:{
+		supplies:[
+			'paper','pencil']
+		location: {
+			row: 3
+			column:2
+		}
+		color: 'red'
+
+	}
+	desk3:{
+		supplies:[
+			'pen','calculator']
+		location: {
+			row: 2
+			column:3
+		}
+		color: 'yellow'
+
+	}
+}
