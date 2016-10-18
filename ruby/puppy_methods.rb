50e35cd6bcf2d51d77bea452446b618b0fcbddc5
Gibral Raphael <gibral.raphael@gmail.com>
2016-10-18 12:16:15 -0400

    added puppy methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,29 @@ class Puppy
     toy
   end
 
+  def speak(num)
+  	num.times do 
+  		puts "Woof!"
+  	end
+  end
+
+  def roll_over()
+  	puts "*RollsOver*"
+  end
+
+  def dog_years(human_years)
+  	puts human_years * 7
+  end
+
+  def dog_eat(food)
+  	puts "#{food} is so tasty"
+  end
+
 end
 
+fido = Puppy.new
+fido.fetch("ball")
+fido.speak(3)
+fido.roll_over 
+fido.dog_years(10)
+fido.dog_eat("steak")
