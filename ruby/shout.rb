59ec987a883e0400c38bb7161368c1266b368df4
Gibral Raphael <gibral.raphael@gmail.com>
2016-10-20 17:11:42 -0400

    added a module and two classes to shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,35 @@
-module Shout
+# module Shout
+# 	def self.yell_angrily(words)
+#     	words + "!!!" + " :("
+# 	end
 
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + " :)"
+# 	end
+# end
+
+# p Shout.yell_angrily("Mad me")
+# p Shout.yelling_happily("grrrr")
+
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!!!!!  :("
+	end
 end
+
+class Lawyer
+	include Shout
+end
+
+class Businessman
+	include Shout
+end
+
+kofi = Lawyer.new
+melody = Businessman.new
+
+p kofi.yell_angrily("grrrr")
+p melody.yell_angrily("haaaaaaa")
+
+
