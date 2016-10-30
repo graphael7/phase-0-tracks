51058dd39d1f2cce9d7e15b0a5445b887f743c13
Gibral Raphael <gibral.raphael@gmail.com>
2016-10-30 15:53:25 -0400

    completed release 0

--- js/algos.js
+++ js/algos.js
@@ -1,10 +1,12 @@
 function LongestPhrase(phrases){
-	
+	var size_of_phrase = phrases.length - 1
 	do{
-		if (phrases[0].length > phrases[1].length){
-			phrases.slice(1);
+		size_of_phrase = phrases.length - 1
+		console
+		if (phrases[0].length > phrases[size_of_phrase].length){
+			phrases.pop();
 		}else {
-			phrases.slice(0);
+			phrases.shift();
 		}
 
 
@@ -13,5 +15,8 @@ function LongestPhrase(phrases){
 }
 
 words = ["long phrase","longest phrase","longer phrase"];
-
+words1 = ["long phraseffhm","longehfmhfmst phrase","longer fhmhfphrase"];
+words2 = ["long hhphrase","longest phmrase","longerxfmmf phrase"];
 console.log (LongestPhrase(words));
+console.log (LongestPhrase(words1));
+console.log (LongestPhrase(words2));
