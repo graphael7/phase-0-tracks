960d59506e2ebdce40504e23fa6d8682e5fb0e91
Gibral Raphael <gibral.raphael@gmail.com>
2016-10-30 16:22:54 -0400

    added release 1

--- js/algos.js
+++ js/algos.js
@@ -14,9 +14,45 @@ function LongestPhrase(phrases){
 	return phrases;
 }
 
+function Match(testvalue1,testvalue2){
+	keys_value1 = Object.keys(testvalue1);
+	keys_value2 = Object.keys(testvalue2);
+	// for (var i = 0; i < keys_value1.length; i++){
+	// 	for (var j = 0; j < keys_value2.length; j++){
+	// 		if (keys_value1[i] == keys_value2[j]){
+	// 		return true;
+	// 		}
+	// 	}
+	// }
+
+	for (var x = 0; x < keys_value1.length; x++){
+		for (var y = 0; y < keys_value2.length; y++){
+			if (testvalue1[keys_value1[x]] == testvalue2[keys_value2[y]]){
+			return true;
+			}
+		}
+	}
+
+	return false
+}
+
 words = ["long phrase","longest phrase","longer phrase"];
 words1 = ["long phraseffhm","longehfmhfmst phrase","longer fhmhfphrase"];
 words2 = ["long hhphrase","longest phmrase","longerxfmmf phrase"];
 console.log (LongestPhrase(words));
 console.log (LongestPhrase(words1));
 console.log (LongestPhrase(words2));
+
+
+entry1 = {name: "Steven", age: 54}
+entry2 = {name: "Tamir", age: 54}
+
+entry3 = {name: "Steven", age: 54}
+entry4 = {name: "Tamir", age: 52}
+
+entry5 = {name: "Tamir", age: 24}
+entry6 = {name: "Tamir", age: 52}
+
+console.log(Match(entry1,entry2));
+console.log(Match(entry3,entry4));
+console.log(Match(entry5,entry6));
